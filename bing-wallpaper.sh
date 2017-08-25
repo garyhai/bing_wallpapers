6effd364a53e17a933fbdfb659458b3be3783846
Gary Hai <gary@XL59.com>
2017-08-26 00:45:57 +0800

    Change the fetching API of bing photos.
    Add -s|--size, -d|--day, -c|--count options.
    And remove -s|--ssl option.

--- bing-wallpaper.sh
+++ bing-wallpaper.sh
@@ -13,7 +13,10 @@ Usage:
 Options:
   -f --force                     Force download of picture. This will overwrite
                                  the picture if the filename already exists.
-  -s --ssl                       Communicate with bing.com over SSL.
+  -s --size                      Size of the photos. [default:1920x1080]
+                                 [1920x1200, 1920x1080, 1366x768]
+  -d --day                       Day of the bing photo before now. [default: 0]
+  -c --count                     Count of photos to fetch. [default: 1]
   -q --quiet                     Do not display log messages.
   -n --filename <file name>      The name of the downloaded picture. Defaults to
                                  the upstream name.
@@ -33,6 +36,10 @@ print_message() {
 
 # Defaults
 PICTURE_DIR="$HOME/Pictures/bing-wallpapers/"
+SIZE="1920x1080"
+DAY="0"
+COUNT="1"
+BING_HOME="https://www.bing.com"
 
 # Option parsing
 while [[ $# -gt 0 ]]; do
@@ -47,11 +54,20 @@ while [[ $# -gt 0 ]]; do
             FILENAME="$2"
             shift
             ;;
+        -s|--size)
+            SIZE="$2"
+            shift
+            ;;
+        -d|--day)
+            DAY="$2"
+            shift
+            ;;
+        -c|--count)
+            COUNT="$2"
+            shift
+            ;;
         -f|--force)
             FORCE=true
-            ;;
-        -s|--ssl)
-            SSL=true
             ;;
         -q|--quiet)
             QUIET=true
@@ -75,31 +91,35 @@ done
 
 # Set options
 [ $QUIET ] && CURL_QUIET='-s'
-[ $SSL ]   && PROTO='https'   || PROTO='http'
 
 # Create picture directory if it doesn't already exist
 mkdir -p "${PICTURE_DIR}"
 
 # Parse bing.com and acquire picture URL(s)
-urls=( $(curl -sL $PROTO://www.bing.com | \
-    grep -Eo "url:'.*?'" | \
-    sed -e "s/url:'\([^']*\)'.*/$PROTO:\/\/bing.com\1/" | \
-    sed -e "s/\\\//g") )
-
+API="${BING_HOME}/HPImageArchive.aspx?format=xml&idx=$DAY&n=$COUNT"
+ACTION="curl -sL \"${API}\" | \
+        ggrep -Po '(?<=\<urlBase\>)(.*?)(?=\</urlBase\>)'"
+urls=( `eval $ACTION`)
 for p in "${urls[@]}"; do
+    url="${BING_HOME}${p}_${SIZE}.jpg"
     if [ -z "$FILENAME" ]; then
-        filename=$(echo "$p"|sed -e "s/.*\/\(.*\)/\1/")
+        filename=$(echo "$url"|sed -e "s/.*\/\(.*\)/\1/")
     else
         filename="$FILENAME"
     fi
     if [ $FORCE ] || [ ! -f "$PICTURE_DIR/$filename" ]; then
         print_message "Downloading: $filename..."
-        curl $CURL_QUIET -Lo "$PICTURE_DIR/$filename" "$p"
+        curl $CURL_QUIET -Lo "$PICTURE_DIR/$filename" "$url"
         "/Applications/Mission Control.app/Contents/MacOS/Mission Control"
-        osascript -e "tell application \"System Events\" to set properties of desktops to {picture rotation:0, picture : \"$PICTURE_DIR/$filename\"}"
+        sleep 1
+        osascript -e "tell application \"System Events\" \
+                      to set properties of desktops to \
+                      {picture rotation:0, \
+                       picture : \"$PICTURE_DIR/$filename\"}"
     else
         print_message "Skipping: $filename..."
         "/Applications/Mission Control.app/Contents/MacOS/Mission Control"
-        osascript -e "tell application \"System Events\" to set picture rotation of desktops to 1"
+        osascript -e "tell application \"System Events\" \
+                      to set picture rotation of desktops to 1"
     fi
 done
